3a7c1414e801ed19cf4313b8d62c4681e6b317ab
alswns123739 <202510703@dong-a.hs.kr>
2025-06-28 03:09:22 +0900

    All

--- # 5장.py
+++ # 5장.py
@@ -86,8 +86,52 @@ for n in range(n, -1, -1):
 # 1개의 정수(integer) (1 ~ 100)를 입력받아 거꾸로 카운트다운하는 프로그램을 작성해봅시다. (0부터 시작해 입력받은 정수까지 1씩 증가시키며 출력합니다.)
 n = int(input())
 for n in range(0, n+1, 1): 
-#변화량 1로 0부터 n까지를 순회하는 반복문을 for문과 range함수를 활용하여 작성해보자
+# 변화량 1로 0부터 n까지를 순회하는 반복문을 for문과 range함수를 활용하여 작성해보자
     print(n)
 
-# while문
-# 조건이 참일때까지 반복
+
+
+# 중첩제어구조
+# 선택구조와 반복구조 2개이상 결합하여 사용하는 구조
+# for문과 if문을 중첩하여 사용하는 코드
+
+# OR
+
+# for문 안에 또 다른 for문 중첩 작성하는 코드
+# for문을 2번 중첩해서 사용하는 것을 이중 for문
+
+# 이중for문 예제
+for i in range(4):
+    for j in range(3):
+        print("i=", i, "j=", j)
+    print("===============")
+
+# 예제1 해설
+# for i in range(4)와 같이 바깥에 있는 for문과 for j in range(3)과 같이 안 쪽에 있는 for문으로 이루어져 있습니다. 바깥쪽 for문이 실행되어 데이터 하나를 i에 저장하면, 그때 안쪽 for문이 실행됩니다. 안쪽 for문의 반복이 모두 끝날 때까지 i는 처음 저장된 그 값을 유지하고, 안쪽 for문 반복이 전부 끝난 후에야 바깥쪽 for문이 다시 실행되어 i의 값이 바뀝니다.
+
+# i가 0일 때 j가 0에서 2까지 반복되고, 그 다음 i가 1일 때 다시 j가 반복되며, 동일한 방식으로 i가 3, j가 2일 때까지 맨 안쪽 코드는 총 12번 반복 실행되고 코드가 종료됩니다.
+
+# 예제 2, for문과 if문을 함께 사용하여 3의 배수만 더해보기
+
+n =input("정수 입력")
+n = int(n)
+sum = 0
+
+# 0에서 n까지 순서대로 i에 대입하고, i가 3의 배수일 때에만 sum에 더해주는 반복문입니다. 
+for i in range(n+1):
+    if i % 3 == 0:
+        sum = sum +i
+print(sum)
+
+
+# 예제3
+# 양의 정수 n을 입력받고 * 표시로 세로 길이 n, 가로 길이 n인 사각형을 그려 봅시다. (사각형 내부도 *로 채워지도록 합니다.) 각 별표는 공백으로 구분되어 출력됩니다
+n = int(input())  # Get input for the size of the pattern
+
+# Loop for the vertical length
+for i in range(n):
+    # Loop for the horizontal length
+    for j in range(n):
+        print("*", end=' ')  # Print '*' without going to the next line
+    print()  # Move to the next line after printing n stars
+
